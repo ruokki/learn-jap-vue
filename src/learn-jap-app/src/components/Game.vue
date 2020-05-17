ebb35b9fe60a7ecd532601a4cb20afc1787b07d8
unknown <fubautsu@gmail.com>
2020-05-17 20:26:43 +0200

    Mise en place de l'affichage des "tuiles réponse"

--- src/learn-jap-app/src/components/Game.vue
+++ src/learn-jap-app/src/components/Game.vue
@@ -1,5 +1,13 @@
 <template>
-  <div  class=""></div>
+    <div>
+        <div v-for="i in setTile" :key="i" class="columns">
+            <div class="column">{{ i }}</div>
+            <div class="column">{{ i + 1 }}</div>
+        </div>
+        <div v-if="nbTile % 2 === 1" class="columns">
+            <div class="column">last</div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -7,7 +15,18 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class Game extends Vue {
-    
+    nbTile = 0;
+
+    data() {
+        return {
+            //nbTile: this.$store.state.tiles
+            nbTile: 4
+        }
+    }
+
+    get setTile(): number {
+        return Math.floor(this.nbTile / 2);
+    }
 }
 </script>
 
