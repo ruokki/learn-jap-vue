0daa9cf7530c74972b9ee084dc0cc8f4e96b5501
unknown <fubautsu@gmail.com>
2020-05-17 17:52:18 +0200

    Ajout header

---
+++ src/learn-jap-app/src/components/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+    <b-navbar type="is-primary">
+        
+    </b-navbar>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Nav extends Vue {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
